812e92c658f83ad93f244cb55bb2e7c2ac73ba08
Benjamin Voelker <voelkerb@informatik.uni-freiburg.de>
2021-10-26 12:24:56 +0200

    docu with examples

--- example.py
+++ example.py
@@ -1,9 +1,11 @@
 # %%
 import helper as hp
 # Set FIRED base Folder (location where you downloaded the dataset)
-hp.FIRED_BASE_FOLDER = "~/FIRED"
+hp.FIRED_BASE_FOLDER = "/Volumes/Data/NILM_Datasets/FIRED"
+# hp.FIRED_BASE_FOLDER = "~/FIRED"
 # load 1Hz power data of the television for complete recording range
 television = hp.getPower("television", 1)
+print(television)
 
 # %%
 # load 2 hours of 50Hz power data of powermeter09 (Fridge) of day 2020.08.03
@@ -26,13 +28,28 @@ fig, ax = plt.subplots()
 ax.plot(dates, fridge["data"]["p"], label="active power")
 ax.plot(dates, fridge["data"]["q"], label="reactive power")
 # format plot  
-ax.set_ylim(0,80)
 ax.set(xlabel='Time of day', ylabel='Power [W/var]', title='Fridge')
 ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
 plt.setp(ax.get_xticklabels(), ha="center", rotation=35)
 plt.legend()
 plt.show()
 
+# %%
+smartmeter = hp.getMeterPower(hp.getSmartMeter(), 50, startTs=startTs, stopTs=stopTs)
+
+# Plot
+fig, axes = plt.subplots(3, sharex=True, title='Fridge')
+for i,ax in enumerate(axes):
+    ax.plot(dates, smartmeter[i]["data"]["p"], label="active power")
+    ax.plot(dates, smartmeter[i]["data"]["q"], label="reactive power")
+# format plot  
+axes[0].set(title='Fridge')
+axes[1].set(ylabel='Power [W/var]')
+axes[-1].set(xlabel='Time of day')
+axes[-1].xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+plt.setp(axes[-1].get_xticklabels(), ha="center", rotation=35)
+plt.legend()
+plt.show()
 # %%
 # Data is now loaded on the fly over rsync
 hp.RSYNC_ALLOWED = True
